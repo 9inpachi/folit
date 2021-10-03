9e399ff4cfc3a1a8eab9ffb96d1ec6794474cfdd
Fawad Ali <m.fawaadali98@gmail.com>
2021-10-03 23:29:25 +0500

    Try smoothing scroll

--- components/horizontal-layout/functions/use-scroll.ts
+++ components/horizontal-layout/functions/use-scroll.ts
@@ -1,8 +1,13 @@
 import { WheelEventHandler } from "react";
 
 export const useScroll = () => {
+  let translateX = 0;
+
   const onWheel: WheelEventHandler<HTMLDivElement> = (event) => {
-    event.currentTarget.scrollLeft += event.deltaY;
+    translateX -= event.deltaX ? event.deltaX : event.deltaY;
+    if (translateX < 0) {
+      event.currentTarget.style.transform = `translateX(${translateX}px)`;
+    }
   };
 
   return { onWheel };
