6ebf994f0b22f82b5732de6c6d028b920eadaa0a
Fawad Ali <m.fawaadali98@gmail.com>
2022-04-11 23:44:44 +0500

    Back to basic horizontal scroll

--- components/horizontal-layout/functions/use-scroll.ts
+++ components/horizontal-layout/functions/use-scroll.ts
@@ -1,48 +1,19 @@
-import { RefObject, useEffect, useState, WheelEventHandler } from "react";
-
-interface Size {
-  width: number;
-  height: number;
-}
+import { RefObject, useEffect } from "react";
 
 export const useScroll = (
   wrapper: RefObject<HTMLDivElement>,
   horizontalScroll: RefObject<HTMLDivElement>
 ) => {
   useEffect(() => {
-    const handleResize = () => {
-      wrapper.current?.style.setProperty(
-        "width",
-        horizontalScroll.current?.getBoundingClientRect().width + "px"
-      );
+    const scrollElement = horizontalScroll.current;
+    if (!scrollElement) return;
+
+    const onWheel = (event: WheelEvent) => {
+      scrollElement.scrollLeft += event.deltaY;
     };
 
-    window.addEventListener("resize", handleResize);
+    scrollElement.addEventListener("wheel", onWheel);
 
-    return () => window.removeEventListener("resize", handleResize);
-  }, [wrapper, horizontalScroll]);
-
-  const scrollInfo = {
-    currentX: 0,
-    ease: 0.1,
-    previous: 0,
-    rounded: 0,
-  };
-
-  const smoothScroll = () => {
-    scrollInfo.currentX = horizontalScroll.current?.scrollLeft ?? 0;
-    scrollInfo.previous +=
-      (scrollInfo.currentX - scrollInfo.previous) * scrollInfo.ease;
-    scrollInfo.rounded = Math.round(scrollInfo.previous * 100) / 100;
-    horizontalScroll.current?.style.setProperty(
-      "transform",
-      `translate3d(-${scrollInfo.rounded}px, 0, 0)`
-    );
-
-    requestAnimationFrame(() => smoothScroll());
-  };
-
-  useEffect(() => {
-    requestAnimationFrame(() => smoothScroll());
-  }, []);
+    return () => scrollElement.removeEventListener("wheel", onWheel);
+  }, [horizontalScroll]);
 };
